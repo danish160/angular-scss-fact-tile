827f516121fe16778d610b5f1a77652e82fbab73
danish160 <danish160@yahoo.com>
2020-02-26 15:35:09 +1100

    code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { NewComponent } from './new/new.component';
 import { FactTileComponent } from './fact-tile/fact-tile.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NewComponent, FactTileComponent ],
+  declarations: [ AppComponent, FactTileComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
